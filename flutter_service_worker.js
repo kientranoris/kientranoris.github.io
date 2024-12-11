aec575a661f4d23609b1608ab1c81876aa196a71
kien.tran.oris <77789220+kientranoris@users.noreply.github.com>
2024-12-11 00:49:47 -0500

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6bf1de17b47de2f35aa91ea299f2316a",
+const RESOURCES = {"flutter_bootstrap.js": "709d8e781384ed8185f086552d362a6a",
 "version.json": "6927f0eaf1ba8326f1d88bd169908f16",
 "index.html": "23a8bbda059e6277bdd619e17eb8ac20",
 "/": "23a8bbda059e6277bdd619e17eb8ac20",
-"main.dart.js": "b0189d76eae1343f4230d05cbf0dd0bc",
+"main.dart.js": "33f862582e19e581bbb9cf4f6b499b97",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
