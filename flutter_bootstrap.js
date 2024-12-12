60cef391352937055b9bc3dcc6ac58abaa69c55d
kien.tran.oris <77789220+kientranoris@users.noreply.github.com>
2024-12-12 09:40:38 -0500

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2580261969"
+    serviceWorkerVersion: "1864786604"
   }
 });
